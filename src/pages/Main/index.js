1698bc53973785ec90526e6a1d26615aaef98b33
Alan Nunes <alanunesouza@hotmail.com>
2019-05-08 21:34:22 -0300

    Adicao de tab

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#FFF" />
+            <Icon name="visibility-off" size={28} color="#FFF" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Alan Henrique Nunes de Souza hoje ás 18:00h.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
